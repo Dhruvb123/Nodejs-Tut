f07e9298204145ac9abd7441ad7d4ea61a6bebc8
Dhruv Bheda <dhruvbheda009@gmail.com>
2025-03-03 21:55:31 +0530

    Middleware Tut Complete

--- 09Middleware/index.js
+++ 09Middleware/index.js
@@ -2,3 +2,28 @@ const express = require("express");
 
 const app = express();
 const PORT = 3000;
+
+// Built-In Middleware
+app.use(express.json());
+
+// Custom Middleware
+app.use((req, res, next) => {
+  console.log("Hi I am Middleware 1");
+  req.newProperty = "Attached By Middleware 1";
+  next();
+
+  // we can even send the response here itself
+  // res.send("Pending");
+});
+
+app.use((req, res, next) => {
+  console.log("Hi I am Middleware 2");
+  console.log("New Property: ", req.newProperty);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Route Reached");
+});
+
+app.listen(3000);
